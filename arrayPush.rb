9edae1d3835b0dd6205abc8478b832065b0ee8f2
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-04-20 14:28:55 +0600

    looping and array

--- arrayPush.rb
+++ arrayPush.rb
@@ -1,7 +1,15 @@
-booksShelf = []
-booksShelf.push(" the creative engineer")
-booksShelf.push("the 4 hours have to work in week")
+# booksShelf = []
+# booksShelf.push(" the creative engineer")
+# booksShelf.push("the 4 hours have to work in week")
 
-print booksShelf[0]  #the creative 
+# print booksShelf[0]  #the creative 
 
-print bookshelf[1] # The 4 hours work week
+# print bookshelf[1] # The 4 hours work week
+
+# The << method is just slightly different:
+
+bookshelf = []
+bookshelf << "Lean Startup"
+bookshelf << "Zero to One"
+print bookshelf[0] # Lean Startup
+print bookshelf[1] # Zero to One
