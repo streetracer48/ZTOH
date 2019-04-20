aba5733e8fe4e3ff17db08720502a5fb8d7415cc
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-04-20 14:26:30 +0600

    array push

---
+++ arrayPush.rb
@@ -0,0 +1,7 @@
+booksShelf = []
+booksShelf.push(" the creative engineer")
+booksShelf.push("the 4 hours have to work in week")
+
+print booksShelf[0]  #the creative 
+
+print bookshelf[1] # The 4 hours work week
