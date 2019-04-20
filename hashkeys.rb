fa4b15e527b3cb6606986c49361684a7d0ba7a19
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-04-20 15:17:34 +0600

    hash key basic

---
+++ hashkeys.rb
@@ -0,0 +1,10 @@
+hash_jhon = {
+    "name" => "doe",
+    "nickname" => "jhon",
+    "nationality" => "bangladeshi",
+    "age" =>24
+}
+
+print "My name is #{hash_jhon["name"]}"
+print "My nickname is #{hash_jhon["nickname"]}"
+print "My age is #{hash_jhon["age"]}"
